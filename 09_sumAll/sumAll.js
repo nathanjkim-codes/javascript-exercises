6ba28d5b9cbd03f86e5cb19b6292bc48292f54e3
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2025-11-01 16:29:05 -0700

    feat: check numbers and swap min/max in sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,13 @@
 const sumAll = function() {
-
+if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  if (min < 0 || max < 0) return "ERROR";
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
