1e03cb212e35246348d59d9e2cfbde83fb1e7da7
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2025-11-02 20:42:43 -0800

    feat: add leap year validation in leapYears function

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  const isYearDivisibleByFour = year % 4 === 0;
+  const isCentury = year % 100 === 0;
+  const isYearDivisibleByFourHundred = year % 400 === 0;
 
+  if (isYearDivisibleByFour && (!isCentury || isYearDivisibleByFourHundred)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
+onsole.log(leapYears(2000)); // true
+console.log(leapYears(1900)); // false
+console.log(leapYears(1984)); // true
+console.log(leapYears(1985)); // false
 // Do not edit below this line
 module.exports = leapYears;
