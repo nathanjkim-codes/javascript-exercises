e5e26610c059e9da5181250ccb7749d3dc9a82fb
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2025-11-03 14:24:46 -0800

    feat: implment temperature conversion functions (Celsius <-> Fahrenheit)

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  let result = ((fahrenheit - 32) * 5) / 9;
+
+  return Math.round(result * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let result = (celsius * 9) / 5 + 32;
+
+  return Math.round(result * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
